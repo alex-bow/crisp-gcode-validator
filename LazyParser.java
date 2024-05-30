64e1ab81ee84bd341d578aa20743db3507ca3ae8
Alex B <145717999+alex-enginseer@users.noreply.github.com>
2024-05-30 13:37:41 -0600

    Parse file into structure of lines

--- LazyParser.java
+++ LazyParser.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class LazyParser {
     Scanner scanner;
@@ -19,9 +20,26 @@ class LazyParser {
 
     void parseGcodeFile() {
         String nextLineStr;
+        String[] words;
+        boolean lineIsComment;
+        Line nextLine;
+        GCodeCommand cmd;
+        int start;
         while (scanner.hasNextLine()) {
             nextLineStr = scanner.nextLine();
-            System.out.println(nextLineStr);
+            if (!nextLineStr.trim().isEmpty()) {
+                lineIsComment = (nextLineStr.substring(0,1).equals(";"));
+                words = nextLineStr.split("[\\s]");
+                if (lineIsComment) {
+                    cmd = null;
+                    start = 0;
+                } else {
+                    cmd = new GCodeCommand(words[0]);
+                    start = 1;
+                }
+                nextLine = new Line(lineIsComment, cmd, 
+                new ArrayList<String>(Arrays.asList(Arrays.copyOfRange(words, start, words.length))));
+            }
         }
     }
 }
